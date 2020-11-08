f9f91638e582ffa4c99dbbc1d18b8ba7a01a43a0
pelmatsidis <pealm@gmail.com>
2020-11-09 00:19:24 +0200

    Minor fix in output in git branch format file

--- check_git_branch_format.sh
+++ check_git_branch_format.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Used with tool like husky to prevent incorrect name in branch policy (befoe commit or push as hook)
+# Used to prevent incorrect name in branch policy (may be used before commit or push as hook)
 # In the below code it will fail if branch name has not "_translations_needed" at the end
 
 # Get current branch name
@@ -8,7 +8,7 @@ branch="$(git rev-parse --abbrev-ref HEAD)";
 valid_branch='^((bugfix|hotfix|feature)\/[-a-zA-Z0-9_+]*(_translations_needed$))'; 
 
 if [[ ! $branch =~ $valid_branch ]]; 
-then echo 'Husky Pre-commit error: Wrong branch name [' $branch ']'; 
+then echo 'Wrong branch name [' $branch ']'; 
 exit 1; 
 fi; 
 exit 0;
